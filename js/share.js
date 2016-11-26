9dd3cf71a0baab30e1c4a18e6f069ba0fa1deca6
deangelo200 <deangeloalmeus@gmail.com>
2016-11-26 12:33:26 -0500

    hard work !

--- js/share.js
+++ js/share.js
@@ -1,11 +1,55 @@
-/*if(localStorage.boxOfFacts==null){
+var menuBar =           '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/favorite.png">'+
+                           '<a href="favorite.html" class="external">Favorite</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/animal.png">'+
+                           '<a href="animal.html" class="external">Animal</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/stars.png">'+
+                           '<a href="favorite.html" class="external">Awesome</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/heart.png">'+
+                           '<a href="body.html" class="external">Body</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/flag.png">'+
+                           '<a href="favorite.html" class="external">Country</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/planet-earth.png">'+
+                           '<a href="favorite.html" class="external">Earth</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/fries.png">'+
+                           '<a href="favorite.html" class="external">Food</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/carriage.png">'+
+                           '<a href="favorite.html" class="external">History</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/dice.png">'+
+                           '<a href="random.html" class="external">Random</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/flask.png">'+
+                           '<a href="favorite.html" class="external">Science</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/team.png">'+
+                           '<a href="favorite.html" class="external">Society</a>'+
+                        '</li>'+
+                         '<li class="side-menu-item">'+
+                          '<img class="menu-icon" src="img/menu/rocket.png">'+
+                           '<a href="favorite.html" class="external">Univers</a>'+
+                        '</li>'
+                         
+                        
+
+
+
+    $(".side-menu").append(menuBar)
    
-    boxOfFacts = {};
-}
-else{
-   boxOfFacts = JSON.parse(localStorage.getItem("boxOfFacts"));
-    
-}*/
-
-
-boxOfFacts = {};
