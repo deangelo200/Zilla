e00a58bfb2ab0a12869fe4e304513fe8b4be5418
deangelo200 <deangeloalmeus@gmail.com>
2016-12-20 02:13:17 -0500

    lkmjb

--- js/africa.js
+++ js/africa.js
@@ -26,7 +26,7 @@ var mySwiper = myApp.swiper('.swiper-container', {
 fastClicks:true
 }); 
 
- $.get('fact-data/africa.txt',function(data){
+ $.get('fact-data/usa.txt',function(data){
     var lines = data.split("~");
     var arraytest = data.split("~").length; 
   
