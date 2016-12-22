1f92b4d92861e74448b053da0241d6a36bbc0ab1
deangelo200 <deangeloalmeus@Deangelos-Macintosh.local>
2016-12-21 21:51:20 -0500

    fucccc

--- js/ad.js
+++ js/ad.js
@@ -1,30 +1,44 @@
  ///// Code for ad 
 
 
-var ad_units = {
+
+function onLoad() {
+			if ((/(ipad|iphone|ipod|android)/i.test(navigator.userAgent))) {
+				document.addEventListener('deviceready', initApp, false);
+			} else {
+				initApp();
+			}
+		}
+		var ad_units = {
+			ios : {
+				banner : "177365",
+				interstitial : "177364"
+			},
 			android : {
 				banner : "234772"
 				
 			}
 		};
-
-//
-   
-var adid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
-
-if(mMedia) mMedia.createBanner({
-        adId : adid.banner,
-        autoShow : true,
-        overlap : true,
-        position : mMedia.AD_POSITION.BOTTOM_CENTER
-    });
-
-
-
-
-$$(document).on('deviceready', function(){ 
-    
-  
-    alert("Working");
-  
-})
+		// select the right Ad Id according to platform
+		var adid = (/(android)/i.test(navigator.userAgent)) ? ad_units.android : ad_units.ios;
+		function initApp() {
+			if (! mMedia) {
+				alert('mMedia plugin not ready');
+				return;
+			}
+			initAd();
+			
+			// display the banner at startup
+			createSelectedBanner();
+		}
+		function initAd() {
+			var defaultOptions = {
+				// adId: adid.banner,
+				position : mMedia.AD_POSITION.BOTTOM_CENTER,
+				// x: integer,		// valid when set position to 0 / POS_XY
+				// y: integer,		// valid when set position to 0 / POS_XY
+				// autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
+			};
+			mMedia.setOptions(defaultOptions);
+			registerAdEvents();
+		}
